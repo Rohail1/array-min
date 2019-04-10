fea4c13cbf8cd29e8cbaa3807f1bdf91458365de
Rohail Najam <rohail_najam@hotmail.com>
2019-04-10 14:59:49 +0500

    Added MIT License

--- setup.py
+++ setup.py
@@ -1,12 +1,17 @@
 from setuptools import setup
 
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
 setup(
     name='array-min',
     version='0.9.1',
     packages=['array-min'],
     url='https://github.com/Rohail1/array-min/',
-    license='Apache 2.0',
+    license='MIT',
     author='Rohail Najam',
     author_email='rohaiil_najam@hotmail.com',
-    description='It is used to find minimum of a monotonically diverging arrays.'
+    description='It is used to find minimum of a monotonically diverging arrays.',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
 )
