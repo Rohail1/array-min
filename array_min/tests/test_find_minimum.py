f6045b6f141ad9bc6036300bb0af3907abd0f0ad
Rohail Najam <rohail_najam@hotmail.com>
2019-04-10 21:51:32 +0500

    bugfixes and handled edge cases.

--- array_min/tests/test_find_minimum.py
+++ array_min/tests/test_find_minimum.py
@@ -5,8 +5,17 @@ from array_min.minimum import find_minimum
 class FindMinimumTest(TestCase):
 
     def testMinimum(self):
+
+        # Duplicate Numbers in series
         self.assertEqual(find_minimum([9, 7, 6, 2, 3, 3, 4, 5, 7]), 2)
+
         self.assertEqual(find_minimum([100, 99, 98, 97, 16, 17, 18, 19, 22]), 16)
         self.assertEqual(find_minimum([55, 54, 53, 53, 51, 60, 62, 63]), 51)
         self.assertEqual(find_minimum([19, 18, 15, 14, 16, 17, 18, 20]), 14)
+
+        # Only Decreasing Series
+        self.assertEqual(find_minimum([19, 18, 15, 14, 13]), 13)
+
+        # Only Increasing Series
+        self.assertEqual(find_minimum([20, 21, 35, 66, 77]), 20)
 
